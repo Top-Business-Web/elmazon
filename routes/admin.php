2383873391ab04e86864bef77cd57d4e38e2278c
Abdallah Mahmoud <Abdalluhmahmoud55@gmail.com>
2023-02-21 09:56:09 +0200

    auth dashboard

---
+++ routes/admin.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Http\Controllers\Admin\MainController;
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\Admin\AuthController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+Route::group(['prefix' => 'admin'], function () {
+    Route::get('login', [AuthController::class, 'index'])->name('admin.dologin');
+    Route::POST('login', [AuthController::class, 'login'])->name('admin.login');
+});
+
+
+Route::group(['prefix' => 'admin', 'middleware' => 'auth:admin'], function () {
+    Route::get('/admin', [MainController::class,'index'])->name('adminHome');
+
+});
+
+
