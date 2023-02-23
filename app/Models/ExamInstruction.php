633756d67d0fb9be7794ef3da64d6c68c4111e6e
eslamandroid12345 <eslamemo457@gmail.com>
2023-02-23 11:28:09 +0200

    update instructions

--- app/Models/ExamInstruction.php
+++ app/Models/ExamInstruction.php
@@ -11,27 +11,32 @@ class ExamInstruction extends Model
 
     protected $guarded = [];
 
-
-    public function all_exam(){
-
-        return $this->belongsTo(AllExam::class,'all_exam_id','id');
+    public function examable()
+    {
+        return $this->morphTo();
     }
 
 
-    public function lesson(){
-
-        return $this->belongsTo(Lesson::class,'lesson_id','id');
-    }
-
-
-    public function video_part(){
-
-        return $this->belongsTo(VideoParts::class,'video_part_id','id');
-    }
-
-    public function subject_class(){
-
-        return $this->belongsTo(SubjectClass::class,'subject_class_id','id');
-    }
+//    public function all_exam(){
+//
+//        return $this->belongsTo(AllExam::class,'all_exam_id','id');
+//    }
+//
+//
+//    public function lesson(){
+//
+//        return $this->belongsTo(Lesson::class,'lesson_id','id');
+//    }
+//
+//
+//    public function video_part(){
+//
+//        return $this->belongsTo(VideoParts::class,'video_part_id','id');
+//    }
+//
+//    public function subject_class(){
+//
+//        return $this->belongsTo(SubjectClass::class,'subject_class_id','id');
+//    }
 
 }
