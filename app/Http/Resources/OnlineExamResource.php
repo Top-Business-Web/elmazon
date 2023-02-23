5309a3343c1752083bf09d4b141cdb35a7ddb561
eslamandroid12345 <eslamemo457@gmail.com>
2023-02-23 14:21:30 +0200

    update exams collection

--- app/Http/Resources/OnlineExamResource.php
+++ app/Http/Resources/OnlineExamResource.php
@@ -18,6 +18,10 @@ class OnlineExamResource extends JsonResource
            'id' => $this->id,
            'name'  => lang() == 'ar' ?$this->name_ar : $this->name_en,
             'note' => $this->note,
+            'season_id' => $this->season_id,
+            'term_id' => $this->term_id,
+            'examable_type' => 	$this->examable_type,
+            'examable_id' => $this->examable_id,
             'created_at' => $this->created_at->format('Y-m-d'),
             'updated_at' => $this->created_at->format('Y-m-d')
 
