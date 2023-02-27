e1a758424be496ac382a66991d7bc1ccb01c6454
eslamandroid12345 <eslamemo457@gmail.com>
2023-02-27 11:56:50 +0200

    add suggest and notification

--- app/Http/Controllers/Api/Auth/AuthController.php
+++ app/Http/Controllers/Api/Auth/AuthController.php
@@ -4,8 +4,13 @@ namespace App\Http\Controllers\Api\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\CommunicationResource;
+use App\Http\Resources\NotificationResource;
+use App\Http\Resources\SuggestResource;
 use App\Http\Resources\UserResource;
+use App\Models\Notification;
 use App\Models\Setting;
+use App\Models\SubjectClass;
+use App\Models\Suggestion;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
@@ -51,6 +56,69 @@ class AuthController extends Controller
 
             return self::returnResponseDataApi(null,$exception->getMessage(),500);
         }
+    }
+
+    public function addSuggest(Request $request)
+    {
+
+        try {
+            $rules = [
+                'suggestion' => 'required|string',
+            ];
+            $validator = Validator::make($request->all(), $rules, [
+                'suggestion.string' => 407,
+            ]);
+
+            if ($validator->fails()) {
+
+                $errors = collect($validator->errors())->flatten(1)[0];
+                if (is_numeric($errors)) {
+
+                    $errors_arr = [
+                        407 => 'Failed,Suggestion must be an a string.',
+                    ];
+
+                    $code = collect($validator->errors())->flatten(1)[0];
+                    return self::returnResponseDataApi(null, isset($errors_arr[$errors]) ? $errors_arr[$errors] : 500, $code);
+                }
+                return self::returnResponseDataApi(null, $validator->errors()->first(), 422);
+            }
+
+            $suggestion = Suggestion::create([
+
+                'user_id' => Auth::guard('user-api')->id(),
+                'suggestion' => $request->suggestion,
+            ]);
+
+            if(isset($suggestion)){
+
+                return self::returnResponseDataApi(new SuggestResource($suggestion),"تم تسجيل الاقتراح بنجاح", 200);
+
+            }
+        } catch (\Exception $exception) {
+
+            return self::returnResponseDataApi(null, $exception->getMessage(), 500);
+        }
+    }
+
+
+    public function allNotifications(){
+
+        try {
+
+            $allNotification = Notification::whereHas('term', function ($term){
+
+                $term->where('status','=','active');
+            })->where('season_id','=',auth()->guard('user-api')->user()->season_id)->get();
+
+            return self::returnResponseDataApi(NotificationResource::collection($allNotification), "تم ارسال اشعارات المستخدم بنجاح", 200);
+
+
+        }catch (\Exception $exception) {
+
+            return self::returnResponseDataApi(null, $exception->getMessage(), 500);
+        }
+
     }
 
     public function communication(){
--- app/Models/Notification.php
+++ app/Models/Notification.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Notification extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+
+    public function term(){
+
+        return $this->belongsTo(Term::class,'term_id','id');
+    }
+
+    public function season(){
+
+        return $this->belongsTo(Season::class,'season_id','id');
+    }
+
+    public function papel_sheet(){
+
+        return $this->belongsTo(PapelSheetExam::class,'papel_sheet_exam_id','id');
+    }
 }
