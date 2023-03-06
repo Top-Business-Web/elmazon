470ffbee53f9d582a0e5d7293b2f4b5ae4db5cdb
eslamandroid12345 <eslamemo457@gmail.com>
2023-03-06 15:24:33 +0200

    update video access next

--- app/Http/Controllers/Api/Question/QuestionController.php
+++ app/Http/Controllers/Api/Question/QuestionController.php
@@ -8,8 +8,10 @@ use App\Http\Resources\QuestionResource;
 use App\Models\Answer;
 use App\Models\OnlineExam;
 use App\Models\OnlineExamQuestion;
+use App\Models\OnlineExamUser;
 use App\Models\Question;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 
 class QuestionController extends Controller{
@@ -42,7 +44,7 @@ class QuestionController extends Controller{
             $rules = [
                 'question_id' => 'required|exists:questions,id',
                 'answer_id' => 'required|exists:answers,id',
-                'status' => 'required|in:solved,leave,un_correct'
+                'status' => 'nullable|in:leave'
             ];
             $validator = Validator::make($request->all(), $rules, [
                 'question_id.exists' => 407,
@@ -57,7 +59,7 @@ class QuestionController extends Controller{
                     $errors_arr = [
                         407 => 'Failed,Question not exists.',
                         408 => 'Failed,Answer not exists.',
-                        409 => 'Failed,Status must be an correct or leave or un_correct in the request.',
+                        409 => 'Failed,Status must be an  leave  in the request.',
                     ];
                     $code = collect($validator->errors())->flatten(1)[0];
                     return self::returnResponseDataApi(null, isset($errors_arr[$errors]) ? $errors_arr[$errors] : 500, $code);
@@ -66,17 +68,32 @@ class QuestionController extends Controller{
             }
 
             $question = Question::where('id', $request->question_id)->first();
-            $answers = Answer::where('question_id',$question->id)->get();
 
-//            foreach ($answers as $answer) {
-//                if($answer->id == $request->answer_id) {
-//
-//                }
-//            }
+//            return $question->answers;
+            foreach ($question->answers as $answer) {
+                if($answer->id == $request->answer_id) {
+                    $online_exam_user = OnlineExamUser::create([
+                        'user_id' => Auth::id(),
+                        'question_id' => $request->question_id,
+                        'answer_id' => $request->answer_id,
+                        'online_exam_id' => $exam->id,
+                        'status' => $answer->status == 'correct' ? 'solved' : 'un_correct',
+                    ]);
+                    return self::returnResponseDataApi(null,"تم حل السؤال واجابتك صحيحه",200);
+                    break;
+                   }else{
+                    $online_exam_user = OnlineExamUser::create([
+                        'user_id' => Auth::id(),
+                        'question_id' => $request->question_id,
+                        'answer_id' => $request->answer_id,
+                        'online_exam_id' => $exam->id,
+                        'status' => $request->status,
+                    ]);
 
-
-            return "Hello";
-
+                    return self::returnResponseDataApi(null,"تم مغادره السؤال وعدم حله",200);
+                    break;
+                }
+            }
 
         }catch (\Exception $exception) {
             return self::returnResponseDataApi(null, $exception->getMessage(), 500);
