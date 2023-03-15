ba3d441a38e9f9372d0be6c07e301209eb4ffb90
eslamandroid12345 <eslamemo457@gmail.com>
2023-03-15 12:01:04 +0200

    update instruction

--- app/Http/Resources/ExamInstructionResource.php
+++ app/Http/Resources/ExamInstructionResource.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources;
 
 use App\Models\AllExam;
+use App\Models\Timer;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class ExamInstructionResource extends JsonResource
@@ -23,11 +24,13 @@ class ExamInstructionResource extends JsonResource
             $type  = 'video';
         }
 
+        $trying = Timer::where('all_exam_id',$this->online_exam_id)->where('user_id','=',auth('user-api')->id())->count();
+        $total_trying = AllExam::where('id','=',$this->online_exam_id)->first();
         return [
 
             'id' => $this->id,
             'instruction' => $this->instruction,
-            'trying_number' => $this->trying_number,
+            'trying_number' =>(int)$total_trying->trying_number - (int)$trying,
             'number_of_question' => $this->number_of_question,
             'quiz_minute' => $this->online_exam->quize_minute,
             'online_exam_id' => $this->online_exam_id,
