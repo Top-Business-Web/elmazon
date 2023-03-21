f774c3884d39ac57b1db2a02d6f99a2f944f618f
eslamandroid12345 <eslamemo457@gmail.com>
2023-03-21 13:58:38 +0200

    add status to lesson and subject_class now with eslam

--- app/Http/Resources/SubjectClassResource.php
+++ app/Http/Resources/SubjectClassResource.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Resources;
 
+use App\Models\OpenLesson;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Auth;
 
 class SubjectClassResource extends JsonResource
 {
@@ -18,6 +20,7 @@ class SubjectClassResource extends JsonResource
 
             'id' => $this->id,
             'name' => lang() == 'ar' ?$this->name_ar : $this->name_en,
+            'status' => OpenLesson::where('user_id','=',Auth::guard('user-api')->id())->where('subject_class_id','=',$this->id)->count() > 0 ? 'opened' : 'lock',
             'image' => $this->image == null ? asset('classes/default/p.png') : asset('classes/' . $this->image),
             'lessons' => LessonResource::collection($this->lessons),
             'exams' => OnlineExamResource::collection($this->exams),
