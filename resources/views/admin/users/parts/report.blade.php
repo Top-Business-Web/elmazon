af91457975065c1c37fad170f0c3ef324940a394
Abdallah Mahmoud <Abdalluhmahmoud55@gmail.com>
2023-04-12 16:09:57 +0200

    make student reports dashboard 12-4-2023

--- resources/views/admin/users/parts/report.blade.php
+++ resources/views/admin/users/parts/report.blade.php
@@ -4,10 +4,9 @@
     تقرير الطالب
 @endsection
 @section('page_name')
-   {{  '  تقرير الطالب :  '. $user->name  }}
+    {{  '  تقرير الطالب :  '. $user->name  }}
 @endsection
 @section('content')
-
 
     <div class="main-content">
 
@@ -15,65 +14,135 @@
             <div class="container-fluid">
                 <div class="row">
                     <div class="col-lg-12">
-                        <div class="card" id="cardPrint">
-                            <div class="card-body">
+                        <div class="card">
+                            <div class="card-body" id="cardPrint">
                                 <div class="invoice-title">
-                                    <h4 class="float-end font-size-16">{{ ' اسم الطالب : ' . $user->name }}</h4>
-                                    <h5 class="float-end font-size-16">الصف الدراسي : </h5>
+                                    <h3 class="float-end font-size-16">{{ ' اسم الطالب : ' . $user->name }}</h3>
+                                    <h6 class="float-end font-size-16">{{ ' كود الطالب : ' . $user->code }} </h6>
+                                    <h6 class="float-end font-size-16">{{ ' الصف الدراسي : ' . $user->season->name_ar }} </h6>
                                 </div>
                                 <hr>
                                 <div class="py-2 mt-3">
-                                    <h3 class="font-size-15 fw-bold">المعلومات</h3>
+                                    <h3 class="font-size-15 col-6 fw-bold">معلومات الاشتراك</h3>
                                 </div>
                                 <div class="table-responsive">
-                                    <table class="table table-nowrap">
+                                    <table class="table table-nowrap table-bordered">
                                         <thead>
                                         <tr>
-                                            <th style="width: 70px;">No.</th>
-                                            <th>Item</th>
-                                            <th class="text-end">Price</th>
+                                            <th style="width: 70px;">#</th>
+                                            <th>الشهر</th>
+                                            <th class="text-end">المبلغ</th>
+                                            <th class="text-end">تاريخ الدفع</th>
                                         </tr>
                                         </thead>
                                         <tbody>
-                                        <tr>
-                                            <td>01</td>
-                                            <td>Skote - Admin Dashboard Template</td>
-                                            <td class="text-end">$499.00</td>
-                                        </tr>
-
-                                        <tr>
-                                            <td>02</td>
-                                            <td>Skote - Landing Template</td>
-                                            <td class="text-end">$399.00</td>
-                                        </tr>
-
-                                        <tr>
-                                            <td>03</td>
-                                            <td>Veltrix - Admin Dashboard Template</td>
-                                            <td class="text-end">$499.00</td>
-                                        </tr>
-                                        <tr>
-                                            <td colspan="2" class="text-end">Sub Total</td>
-                                            <td class="text-end">$1397.00</td>
-                                        </tr>
-                                        <tr>
-                                            <td colspan="2" class="border-0 text-end">
-                                                <strong>Shipping</strong></td>
-                                            <td class="border-0 text-end">$13.00</td>
-                                        </tr>
-                                        <tr>
-                                            <td colspan="2" class="border-0 text-end">
-                                                <strong>Total</strong></td>
-                                            <td class="border-0 text-end"><h4 class="m-0">$1410.00</h4></td>
-                                        </tr>
+                                        @foreach($subscriptions as $subscription)
+                                            <tr>
+                                                <td>{{ $loop->iteration }}</td>
+                                                <td>{{ $subscription->month }}</td>
+                                                <td class="text-end">{{ $subscription->price }}</td>
+                                                <td>{{ $subscription->created_at->format('Y-m-d') }}</td>
+                                            </tr>
+                                        @endforeach
                                         </tbody>
                                     </table>
                                 </div>
-                                <div class="d-print-none">
-                                    <div class="float-end">
-                                        <a style="color: white" onclick="printData()" class="btn btn-success waves-effect waves-light me-1 printReport"><i class="fa fa-print"></i> طباعة </a>
-                                        <a href="{{ route('users.index') }}" class="btn btn-primary w-md waves-effect waves-light">الرجوع</a>
-                                    </div>
+                                <hr>
+                                <div class="py-2 mt-3">
+                                    <h3 class="font-size-15 col-6 fw-bold">معلومات المشاهدة</h3>
+                                </div>
+                                <div class="table-responsive">
+                                    <table class="table table-nowrap table-bordered">
+                                        <thead>
+                                        <tr>
+                                            <th style="width: 70px;">#</th>
+                                            <th>اسم الفيديو</th>
+                                            <th class="text-end">وقت المشاهدة</th>
+                                            <th class="text-end">مدة الفيديو</th>
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+                                        @foreach($videos as $video)
+                                            <tr>
+                                                <td>{{ $loop->iteration }}</td>
+                                                <td>{{ $video->video->name_ar }}</td>
+                                                <td class="text-end">{{ $video->created_at->format('Y-m-d') }}</td>
+                                                <td>{{ $video->video->video_time . ' دقيقة ' }}</td>
+                                            </tr>
+                                        @endforeach
+                                        </tbody>
+                                        <tfoot>
+                                        <td></td>
+                                        <td>المجموع :
+                                            {{ $videos->count() . '  فيديو  ' }}
+                                        </td>
+                                        <td></td>
+                                        <td>مجموع دقائق المشاهده :
+                                        {{ $videoMin . ' دقيقة ' }}
+                                        </td>
+                                        </tfoot>
+                                    </table>
+                                </div>
+                                <hr>
+                                <div class="py-2 mt-3">
+                                    <h3 class="font-size-15 col-6 fw-bold">معلومات الامتحانات</h3>
+                                </div>
+                                <div class="table-responsive">
+                                    <table class="table table-nowrap table-bordered">
+                                        <thead>
+                                        <tr>
+                                            <th style="width: 70px;">#</th>
+                                            <th>اسم الامتحان</th>
+                                            <th>نوع الامتحان</th>
+                                            <th>درجة الامتحان</th>
+                                            <th>تاريخ الامتحان</th>
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+                                        @foreach($exams as $exam)
+                                            <tr>
+                                                <td>{{ $loop->iteration }}</td>
+                                                <td>{{ $exam->online_exam->name_ar ?? $exam->all_exam->name_ar ?? $exam->life_exam->name_ar }}</td>
+                                                @if($exam->online_exam_id)
+                                                    <td>اونلاين</td>
+                                                @elseif($exam->all_exam_id)
+                                                    <td>شامل</td>
+                                                @else
+                                                    <td>لايف</td>
+                                                @endif
+                                                <td>{{ ($exam->online_exam->degree ?? $exam->all_exam->degree ?? $exam->life_exam->degree) }}
+                                                    / {{ $exam->full_degree }}</td>
+                                                <td>{{ $exam->created_at->format('Y-m-d') }}</td>
+                                            </tr>
+                                        @endforeach
+                                        @foreach($paperExams as $paper)
+                                            <tr>
+                                                <td>{{ $loop->iteration }}</td>
+                                                <td>{{ $paper->papel_sheet_exam->name_ar }}</td>
+                                               <td>ورقي</td>
+                                                <td>{{ $paper->papel_sheet_exam->degree }}
+                                                    / {{ $paper->degree }}</td>
+                                                <td>{{ $paper->created_at->format('Y-m-d') }}</td>
+                                            </tr>
+                                        @endforeach
+                                        </tbody>
+                                        <tfoot>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                        <td> مجموع الامتحانات :</td>
+                                        <td>{{ $exams->count() + $paperExams->count() }}</td>
+                                        </tfoot>
+                                    </table>
+                                </div>
+                            </div>
+                            <div class="d-print-none m-3">
+                                <div class="float-end">
+                                    <a style="color: white" onclick="printData()"
+                                       class="btn btn-success waves-effect waves-light me-1 printReport"><i
+                                            class="fa fa-print"></i> طباعة </a>
+                                    <a href="{{ route('users.index') }}"
+                                       class="btn btn-primary w-md waves-effect waves-light">الرجوع</a>
                                 </div>
                             </div>
                         </div>
@@ -85,27 +154,26 @@
         </div>
 
 
-    @include('admin.layouts_admin.myAjaxHelper')
-@endsection
-@section('ajaxCalls')
-    <script>
-        // $('.printReport').on('click', function(e){
-        //     e.preventDefault();
-        //     window.printThis()
-        // })
+        @include('admin.layouts_admin.myAjaxHelper')
+        @endsection
+        @section('ajaxCalls')
+            <script>
+                function printData() {
+                    var css = '<link href="{{asset('assets/admin/plugins/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet" />';
+                    css += '<link href="{{asset('assets/admin/css-rtl/style.css')}}" rel="stylesheet"/>';
+                    var printContent = document.getElementById("cardPrint");
 
-        function printData()
-        {
-            var divToPrint=document.getElementById("cardPrint");
-            newWin= window.open();
-            newWin.document.write(divToPrint.outerHTML);
-            newWin.print();
-            newWin.close();
-        }
+                    var WinPrint = window.open('', '', 'width=1024,height=1024');
+                    WinPrint.document.write('<html lang="en" dir="rtl">');
+                    WinPrint.document.write(printContent.innerHTML);
+                    WinPrint.document.head.innerHTML = css;
+                    WinPrint.document.URL = ''
+                    WinPrint.document.close();
+                    WinPrint.focus();
+                    WinPrint.print();
+                    WinPrint.close();
+                }
 
-        // $('button').on('click',function(){
-        //     printData();
-        // })
-    </script>
+            </script>
 @endsection
 
