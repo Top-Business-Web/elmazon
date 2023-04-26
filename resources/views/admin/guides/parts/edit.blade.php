55ccb3c2b6dde22ce710fd3b079d1fa5e5a8ccbe
abdullah <abdullahalhomsi520@gmail.com>
2023-04-26 14:45:52 +0200

    last edit in guide

--- resources/views/admin/guides/parts/edit.blade.php
+++ resources/views/admin/guides/parts/edit.blade.php
@@ -16,6 +16,24 @@
             </div>
             <div class="row">
                 <div class="col-md-6">
+                    <label for="name_ar" class="form-control-label">الصف</label>
+                    <Select name="season_id" id="season_id" class="form-control season_id" required>
+                        <option selected disabled style="text-align: center">اختار الصف</option>
+                        @foreach($seasons as $season)
+                            <option value="{{ $season->id }}" {{ $guide->season_id == $season->id ? 'selected' : '' }}
+                                    style="text-align: center">{{ $season->name_ar }}</option>
+                        @endforeach
+                    </Select>
+                </div>
+                <div class="col-md-6">
+                    <label for="">الترم</label>
+                    <select name="term_id" id="term_id" class="form-control term_id" required>
+                        <option value="" style="text-align: center">الكل</option>
+                    </select>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6">
                     <label for="section_name_ar" class="form-control-label">الوصف بالعربية</label>
                     <textarea class="form-control" name="description_ar" rows="8"
                               required>{{ $guide->description_ar }}</textarea>
@@ -42,4 +60,21 @@
 
 <script>
     $('.dropify').dropify()
+
+    $(document).ready(function () {
+        $('.season_id').on('change', function () {
+            let season = $(this).val();
+            $.ajax({
+                url: '{{ route("subjectClassSort")}}',
+                method: 'GET',
+                data: {
+                    'id': season,
+                }, success: function (data) {
+                    $('.term_id').html(data);
+                    console.log(data);
+                }
+            })
+        })
+    });
+
 </script>
