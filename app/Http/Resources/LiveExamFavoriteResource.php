797ef9b11f283decbffdfdd31fde4fc6e79bcdf1
Top Business <eslamemo457@gmail.com>
2023-09-19 12:44:31 +0200

    update live exam now

--- app/Http/Resources/LiveExamFavoriteResource.php
+++ app/Http/Resources/LiveExamFavoriteResource.php
@@ -22,13 +22,10 @@ class LiveExamFavoriteResource extends JsonResource
             'name'  => lang() == 'ar' ?$this->name_ar : $this->name_en,
             'exams_favorite' => !ExamsFavorite::where('life_exam_id','=',$this->id)
                 ->where('user_id','=',Auth::guard('user-api')->id())->first()
-            || ExamsFavorite::where('life_exam_id','=',$this->id)->where('user_id','=',Auth::guard('user-api')->id())
+            || ExamsFavorite::where('life_exam_id','=',$this->id)
+                ->where('user_id','=',Auth::guard('user-api')->id())
                 ->where('action','=','un_favorite')->first() ? 'un_favorite' : 'favorite',
-            'answer_pdf_file' => $this->answer_pdf_file != null ? asset('live_exam_file_uploads/pdf/'. $this->answer_pdf_file) : null,
             'answer_video_file' => $this->answer_video_file != null ? asset('live_exam_file_uploads/videos/'. $this->answer_video_file) : null,
-            'num_of_question' => $this->questions->count(),
-            'answer_pdf_size' => 900,
-            'answer_video_size' => 500,
 
 
         ];
