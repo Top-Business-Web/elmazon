ccf20c904ff2c4c1fac2c202f751b61fa64354ea
Top Business <eslamemo457@gmail.com>
2023-09-25 10:45:25 +0200

    update LessonDetailController 5

--- app/Http/Controllers/Api/LessonDetails/LessonDetailsController.php
+++ app/Http/Controllers/Api/LessonDetails/LessonDetailsController.php
@@ -137,12 +137,6 @@ class LessonDetailsController extends Controller{
                 ->count();
 
 
-            $onlineExamUserLeaveAnswers  = OnlineExamUser::query()
-                ->where('user_id','=',Auth::guard('user-api')->id())
-                ->where('online_exam_id','=',$exam->id)
-                ->where('status','=','leave')
-                ->count();
-
 
 
             $tryingNumber = Timer::query()
@@ -154,7 +148,7 @@ class LessonDetailsController extends Controller{
             $data['full_degree']     = ($degree->full_degree) . " / " . $exam->degree;
             $data['motivational_word'] = "ممتاز بس فيه أحسن ";
             $data['correct_numbers'] =  $onlineExamUserCorrectAnswers;
-            $data['mistake_numbers'] =  ($onlineExamUserMistakeAnswers + $onlineExamUserLeaveAnswers);
+            $data['mistake_numbers'] =  $onlineExamUserMistakeAnswers;
             $data['trying_numbers']  =  $tryingNumber;
             $data['exam_questions']  = new ExamQuestionsNewResource($exam);
 
