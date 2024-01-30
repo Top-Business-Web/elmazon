983952f640526e81e3f5491d5ed3011f1d90cff2
eldapour <abdallahmahmoud1422003@gmail.com>
2024-01-30 11:16:30 +0200

    add file manager  30_1_2024

--- resources/views/admin/file_manager/index.blade.php
+++ resources/views/admin/file_manager/index.blade.php
@@ -11,8 +11,8 @@
 {{--    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">--}}
     <link rel="stylesheet" href="{{ asset('vendor/file-manager/css/file-manager.css') }}">
 <style>
-    #fm{
-        height: 900px !important;
+    .fm{
+        height: 600px !important;
     }
 </style>
     <div id="fm"></div>
