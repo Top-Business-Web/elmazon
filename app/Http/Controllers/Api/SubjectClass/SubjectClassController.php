a02dea1e4a440192dd84b65b36e2dc22001e70a6
eldapour <abdallahmahmoud1422003@gmail.com>
2024-03-10 11:18:59 +0200

    add update subscribtion month version

--- app/Http/Controllers/Api/SubjectClass/SubjectClassController.php
+++ app/Http/Controllers/Api/SubjectClass/SubjectClassController.php
@@ -13,76 +13,85 @@ use App\Models\AllExam;
 use App\Models\Lesson;
 use App\Models\OnlineExam;
 use App\Models\SubjectClass;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\JsonResponse;
 
 class SubjectClassController extends Controller
 {
-    public function allClasses(){
+    public function allClasses()
+    {
 
 
         try {
 
-            $classes = SubjectClass::whereHas('term', function ($term){
+            $classes = SubjectClass::whereHas('term', function ($term) {
 
-                $term->where('status', '=', 'active')->where('season_id','=',auth('user-api')->user()->season_id);
-            })->where('season_id','=',auth()->guard('user-api')->user()->season_id)->get();
+                $term->where('status', '=', 'active')->where('season_id', '=', auth('user-api')->user()->season_id);
+            })->where('season_id', '=', auth()->guard('user-api')->user()->season_id)->get();
 
-//            $fullExams = AllExam::whereHas('term', function ($term){
-//
-//                $term->where('status', '=', 'active')->where('season_id','=',auth('user-api')->user()->season_id);
-//            })->where('season_id','=',auth()->guard('user-api')->user()->season_id)->get();
+            //            $fullExams = AllExam::whereHas('term', function ($term){
+            //
+            //                $term->where('status', '=', 'active')->where('season_id','=',auth('user-api')->user()->season_id);
+            //            })->where('season_id','=',auth()->guard('user-api')->user()->season_id)->get();
 
 
-            $subject_classes_all_exams = SubjectClass::whereHas('term', function ($term){
+            $subject_classes_all_exams = SubjectClass::whereHas('term', function ($term) {
 
-                $term->where('status', '=', 'active')->where('season_id','=',auth('user-api')->user()->season_id);
-            })->where('season_id','=',auth()->guard('user-api')->user()->season_id)->whereHas('all_exams')->get();
+                $term->where('status', '=', 'active')->where('season_id', '=', auth('user-api')->user()->season_id);
+            })->where('season_id', '=', auth()->guard('user-api')->user()->season_id)->whereHas('all_exams')->get();
 
 
             return response()->json([
 
                 'data' => [
-                     'subject_class' => SubjectClassNewResource::collection($classes),
-//                     'fullExams' => AllExamResource::collection($fullExams),
-                     'classes_all_exams' => SubjectClassAllExamResource::collection($subject_classes_all_exams),
+                    'subject_class' => SubjectClassNewResource::collection($classes),
+                    //                     'fullExams' => AllExamResource::collection($fullExams),
+                    'classes_all_exams' => SubjectClassAllExamResource::collection($subject_classes_all_exams),
 
                     'code' => 200,
                     'message' => "تم الحصول علي جميع الدروس التابعه لهذا الفصل",
                 ]
             ]);
+        } catch (\Exception $exception) {
 
-
-        }catch (\Exception $exception) {
-
-            return self::returnResponseDataApi(null,$exception->getMessage(),500);
+            return self::returnResponseDataApi(null, $exception->getMessage(), 500);
         }
-
     }
 
-    public function lessonsByClassId($id): JsonResponse{
+    public function lessonsByClassId($id): JsonResponse
+    {
 
         try {
-            $class = SubjectClass::find($id);
+            $user = studentAuth();
+            $class = SubjectClass::query()
+                ->where('id', $id)
+                ->whereHas(
+                    'lessons.videos',
+                    function (Builder $builder) use ($user) {
+                        $builder->whereIn('month', json_decode($user->subscription_months_groups));
+                    }
+                )->with(['lessons' => function ($query) use ($user) {
+                    // Include only lessons that have videos in the same month as the subscription
+                    $query->whereHas('videos', function (Builder $builder) use ($user) {
+                        $builder->whereIn('month', json_decode($user->subscription_months_groups));
+                    });
+                }])->first();
 
-            if(!$class){
-                return self::returnResponseDataApi(null,"هذا الفصل غير موجود",404);
-
+            if (!$class) {
+                return self::returnResponseDataApi(null, "هذا الفصل غير موجود", 404);
             }
 
             return response()->json([
-
                 'data' => [
                     'class' => new SubjectClassNewResource($class),
                     'lessons' => LessonNewResource::collection($class->lessons),
                 ],
                 'code' => 200,
-                'message' => "تم الحصول علي جميع الدروس التابعه لهذا الفصل",
+                'message' => "تم الحصول على جميع الدروس التابعة لهذا الفصل",
             ]);
-
-
-        }catch (\Exception $exception) {
-
-            return self::returnResponseDataApi(null,$exception->getMessage(),500);
+        } catch (\Exception $exception) {
+            return self::returnResponseDataApi(null, $exception->getMessage(), 500);
         }
+
     }
 }
